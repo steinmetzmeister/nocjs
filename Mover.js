1b2d9ede8543a052c9b414dd447a05bf7401dcf0
James Steinmetz <robotsdanceincolor@gmail.com>
2014-05-30 00:31:13 -0700

    Updated Mover added Util/Random

--- Mover.js
+++ Mover.js
@@ -1,12 +1,29 @@
 var Mover = function() {
-    this.location = new Vector2(20, 20);
+    this.loc = new Vector2(Util.randomInt(0, width), Util.randomInt(0, height));
 }
 Mover.prototype = {
-    display: function()
-    {
+    display: function() {
         context.beginPath();
-        context.arc(0, 0, 10, 0, Math.PI * 2, true);
+        // context.arc(this.loc.x, this.loc.y, 10, 0, Math.PI * 2, true);
+        context.arc(this.loc.x, this.loc.y, 10, 0, Math.PI * 2, true);
         context.closePath();
         context.stroke();
+    },
+
+    moveRandom: function() {
+        this.loc.x += Util.randomInt(-1, 1);
+        this.loc.y += Util.randomInt(-1, 1);
+
+        if (this.loc.x > width)
+            this.loc.x = 0;
+
+        if (this.loc.x < 0)
+            this.loc.x = width;
+
+        if (this.loc.y > height)
+            this.loc.y = 0;
+
+        if (this.loc.y < 0)
+            this.loc.y = height;
     }
 }
