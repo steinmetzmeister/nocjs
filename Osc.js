5d6a7d4b8df24fdf9ffdc77335687224890d175e
James Steinmetz <robotsdanceincolor@gmail.com>
2014-06-03 23:49:31 -0700

    Osc test

--- Osc.js
+++ Osc.js
@@ -1,11 +1,20 @@
-var Osc = function() {
+var Osc = function(x, y) {
     this.angle = new Vector2(0, 0);
-    this.velocity = new Vector2(Util.random(-0.1, 0.1), Util.random(-0.1, 0.1));
-    this.amp = new Vector2(Util.randomInt(0, width / 2), Util.randomInt(0, height / 2));
+    
+    if (x && y)
+    {
+        this.velocity = new Vector2(x, y);
+        this.amp = new Vector2(width / 2, height / 2);
+    }
+    else
+    {
+        this.velocity = new Vector2(Util.random(-0.1, 0.1), Util.random(-0.1, 0.1));
+        this.amp = new Vector2(Util.randomInt(0, width / 2), Util.randomInt(0, height/ 2));
+    }
 
     this.color = Util.randomColor();
 
-    this.radius = Util.randomInt(5, 25);
+    this.radius = Util.randomInt(5, 10);
 }
 Osc.prototype = {
     oscillate: function() {
