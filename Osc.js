e55e15bbabb5fe8e4e6efbe33925bf682f3f2986
James Steinmetz <robotsdanceincolor@gmail.com>
2014-06-05 20:22:30 -0700

    More o/c tests

--- Osc.js
+++ Osc.js
@@ -1,11 +1,21 @@
 var Osc = function(x, y) {
-    this.iter = Util.randomInt(1, 10);
+    this.t = 0.01;
+
+    this.height = Util.randomInt(height / 8, height);
+
+    this.speed = Util.random(0.01, 0.02);
+
+    this.dir = 1;
+    if (Math.random() < 0.5)
+        this.dir = -1;
+
+    this.iter = Util.randomInt(1, 2);
 
     // this.angle = new Vector2(0, 0);
-    this.startAngle = 0;
+    this.startAngle = 0
     this.angle = 0;
 
-    this.aVelocity = Util.random(-0.01, 0.01);
+    this.aVelocity = Util.random(-0.025, 0.025);
     
     if (x && y)
     {
@@ -18,7 +28,13 @@ var Osc = function(x, y) {
         this.amp = new Vector2(Util.randomInt(0, width / 2), Util.randomInt(0, height/ 2));
     }
 
-    this.color = Util.randomColor();
+    var rgba = "rgba(" +
+        Util.randomInt(0, 255) + ',' +
+        Util.randomInt(0, 255) + ',' +
+        Util.randomInt(0, 255) + ',' +
+        Util.random(0.9, 1) + ');';
+
+    this.color = rgba;
 
     this.radius = Util.randomInt(5, 10);
 }
@@ -29,20 +45,42 @@ Osc.prototype = {
 
     display: function() {
 
-        this.angle = this.startAngle;
-        this.startAngle += 0.02;
+        var n = map_range(noise.perlin2(0, this.t), -1, 1, -0.1, 0.1); this.t += this.speed;
+        this.aVelocity += n * 0.01;
 
-        if (this.startAngle > Math.PI * 1.125)
-            paused = true;
+        this.angle = this.dir * this.startAngle;
+        this.startAngle += n + 0.025;
 
-        for (var x = 0; x < width; x += this.iter)
+        // if (this.startAngle > Math.PI * 1.125)
+            // paused = true;
+
+        this.height += n * 180;
+        if (this.height > height)
+            this.height = height;
+
+        if (this.height <= 0)
+            this.height = 1;
+
+        var mod = 10;
+        for (var x = 0; x < width; x += 1)
         {
-            var y = map_range(Math.sin(this.angle), -1, 1, 0, height);
+            var y = map_range(Math.sin(this.angle), -1, 1, 0, this.height);
+            var d = height - this.height;
+            y += d / 2;
 
-            ctx.fillStyle = this.color;
-            ctx.fillRect(x, y, 1, 1);
+            if (x % Util.randomInt(0, 32) === 0)
+                y += Util.randomInt(2, 6)  * Util.randomInt(-1, 1);
 
-            this.angle += this.aVelocity;            
+            var r = (Math.random() < 0) ? -1 : 1;
+            // y += Util.randomInt(-2, 2)  * r;
+
+            if (1) // if (x % this.iter === 0)
+            {
+                ctx.fillStyle = this.color;
+                ctx.fillRect(x, y, 1, 1);
+            }
+
+            this.angle += this.aVelocity;
         }
         
         // var x = Math.sin(this.angle.x) * this.amp.x;
---
+++ Pendulum.js
@@ -0,0 +1,20 @@
+var Pendulum = function() {
+
+}
+Pendulum.prototype = {
+
+}
+
+var Spring = function() {
+
+}
+Spring.prototype = {
+    
+}
+
+var Bob = function() {
+
+}
+Bob.prototype = {
+
+}
